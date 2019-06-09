03cf6b4714e6ec6eff52cd3dab97006fcbfba0ed
chelosilverro78 <marcesilvero@gmail.com>
2019-06-09 10:49:15 -0400

    mi primer commit

---
+++ 01/holamundo.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("hola mundo...\n")
+	fmt.Println("hola de nuevo")
+  //para ejecutar $go run holamundo.go   --> esto lo compila y lo ejecuta en memoria sin convertirlo a exe
+  //para construir un binario $go build holamundo.go  --> holamundo.exe
+}
